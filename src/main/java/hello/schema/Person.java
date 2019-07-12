b0b42ff36c41a90d5fa9a28d54798b86c7b08d6f
Alejandro Guzman <noemail@example.com>
2019-07-12 15:37:58 -0700

    Enable lombok and generate the appropriate setters/getters and toString, equalsAndHashCode

--- src/main/java/hello/schema/Person.java
+++ src/main/java/hello/schema/Person.java
@@ -1,5 +1,12 @@
 package hello.schema;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
     private String firstName;
     private String lastName;
