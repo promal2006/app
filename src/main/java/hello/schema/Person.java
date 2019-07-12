e3b5e6ca1c54b4f0a2a98e8f732ce6c9c9ef3812
Alejandro Guzman <noemail@example.com>
2019-07-12 15:41:14 -0700

    modify Person class and make it a JPA entity

--- src/main/java/hello/schema/Person.java
+++ src/main/java/hello/schema/Person.java
@@ -4,12 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue
+    private Long id;
+    
     private String firstName;
+
     private String lastName;
+
     private int age;
+
     private String email;
 }
